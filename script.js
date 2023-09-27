1a907017f4b835db56dd52233c2b1dc2f2b38386
Mirtel Mürk <mirtel.myrk@tptlive.ee>
2023-09-27 14:08:45 +0300

    Add strings and numbers example

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log ('Hello world!');
+
+let num = 1;
+num = 3;
+num += 3;
+num++;
+num = num**2;
+num = 13;
+num = num %2;
+// It will use the X ?/number ? number value, example 6 1/2 would be 1
+num %= 2;
+//var text = 'Hello'; Dont use var
+console.log(num);
+let string = 'hello';
+// soovitatud kasutada ' märke, kui just lauses ei ole ' näiteks don't, ss kasutada ""
+string = "hello";
+string = `hello
+multi line
+string`;
+string = `hello number is: ${num}`
+string = 'hello number is: '+ num;
