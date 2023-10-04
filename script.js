9d827dcca338963c82b3c5bcf657e73903f8129e
Mirtel Mürk <mirtel.myrk@tptlive.ee>
2023-10-04 14:10:47 +0300

    arrays and objects

--- script.js
+++ script.js
@@ -19,3 +19,43 @@ multi line
 string`;
 string = `hello number is: ${num}`
 string = 'hello number is: '+ num;
+
+let boolean = true;
+console.log(boolean);
+boolean = false;
+console.log(boolean);
+boolean = true || false || true;
+console.log(boolean);
+boolean = !true;
+console.log(boolean);
+boolean = true && false || true && !false;
+console.log(boolean);
+num = 10;
+boolean = num > 10;
+boolean = num < 10;
+boolean = num == 10;
+console.log(boolean);
+boolean = num >= 10;
+console.log(boolean);
+if(num == 10) {
+    console.log('võrdne kümnega');
+} else {
+    console.log('ei ole võrdne kümnega');
+}
+console.log(Boolean(num));
+console.log(Boolean('sfakoksf'));
+console.log(Boolean(0));
+console.log(Boolean(''));
+
+let noValue = null;
+let noValue2;
+console.log(noValue);
+console.log(noValue2);
+num = 1/0;
+console.log(num);
+num = -1/0;
+console.log(num);
+
+let array = [1, 2, true, 1.2, 'hello'];
+console.log(array);
+console.log(array[4])
