006d73a880d814cabb049d558a97891af38e5bb6
Mirtel Mürk <mirtel.myrk@tptlive.ee>
2023-10-18 13:54:46 +0300

    ülesanne 2

--- ylesanne1/script.js
+++ ylesanne1/script.js
@@ -1,11 +1,12 @@
-for(let i=1; i<=200; i++) {
-    if(i%5===0 && i%7===0){
-        console.log('FooBar')
-    } else if(i%5===0){
-        console.log('Foo')
-    } else if(i%7===0){
-        console.log('Bar')
-    } else {
-        console.log(i)
+let num1 = 0;
+let num2 = 1;
+let sum = 0;
+while(num2<4_000_000){
+    if(num2%2===0) {
+        sum+=num2;
     }
+    let temp = num2;
+    num2= num1+num2;
+    num1 = temp;
 }
+console.log(sum);
