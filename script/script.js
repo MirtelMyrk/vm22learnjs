4880d1d0fdee2c68c7b43f303591c9bd6a355dc5
Mirtel Mürk <mirtel.myrk@tptlive.ee>
2023-10-18 13:46:57 +0300

    jah

--- script/script.js
+++ script/script.js
@@ -1,4 +1,39 @@
-let num = 1
+let num = 10;
 if(num > 10){
     console.log('suurem');
+} else if(num === 10) {
+    console.log('võrdne');
+} else {
+    console.log('väiksem');
 }
+
+let day = new Date().getDay();
+console.log(day);
+
+
+switch(day){
+    case 0:
+        console.log('Pühapäev');
+        break;
+    case 1:
+        console.log('Esmaspäev');
+        break;
+    case 2:
+         console.log('Teisipäev');
+         break;
+    case 3:
+         console.log('Kolmapäev');
+         break;
+    case 4:
+         console.log('Neljapäev');
+         break;
+    case 5:
+         console.log('Reede');
+         break;
+    case 6:
+         console.log('Laupäev');
+         break;
+    default:
+        console.log('weirdday');
+}
+// kui break; pole ss ta jätkab lugemist kuni tuleb break
